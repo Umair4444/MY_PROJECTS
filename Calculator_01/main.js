9c3c693cfc9c456a236616d30df09fc58fb09c1e
Umair4444 <theumairkhan44@gmail.com>
2024-03-10 13:32:18 +0500

    importing chalk animation

--- Calculator_01/main.js
+++ Calculator_01/main.js
@@ -1,4 +1,10 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
+async function startCalculator() {
+    let c_animation = chalk.green("Let's get Started");
+    console.log(c_animation);
+}
+startCalculator();
 let user_input1 = await inquirer.prompt({
     type: "number",
     name: "number1",
@@ -13,7 +19,14 @@ let user_input3 = await inquirer.prompt({
     type: "list",
     name: "operator",
     message: "enter your operator",
-    choices: ["addition", "substraction", "multiplication", "division"],
+    choices: [
+        "addition",
+        "substraction",
+        "multiplication",
+        "division",
+        "exponent",
+        "reminder",
+    ],
 });
 function calculate(user_input1, user_input2, user_input3) {
     if (user_input3 === "addition") {
@@ -28,15 +41,21 @@ function calculate(user_input1, user_input2, user_input3) {
     else if (user_input3 === "division") {
         console.log(user_input1 / user_input2);
     }
+    else if (user_input3 === "exponent") {
+        console.log(user_input1 ** user_input2);
+    }
+    else if (user_input3 === "reminder") {
+        console.log(user_input1 % user_input2);
+    }
     else {
         console.log("not a valid operation");
     }
 }
 // input no :- 1
-console.log(user_input1.number1);
-// input no :- 2 
-console.log(user_input2.number2);
+console.log("First Number is:", user_input1.number1);
+// input no :- 2
+console.log("Second Number is:", user_input2.number2);
 // your operator.
-console.log(user_input3.operator);
+console.log("The Operator is:", user_input3.operator);
 // your answer
-calculate(user_input1.number1, user_input2.number2, user_input3.operator);
+const res = calculate(user_input1.number1, user_input2.number2, user_input3.operator);
--- Calculator_01/main.ts
+++ Calculator_01/main.ts
@@ -1,4 +1,13 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+async function startCalculator() {
+  let c_animation = chalk.green("Let's get Started");
+  console.log(c_animation);
+}
+
+startCalculator();
 
 let user_input1 = await inquirer.prompt({
   type: "number",
@@ -14,7 +23,14 @@ let user_input3 = await inquirer.prompt({
   type: "list",
   name: "operator",
   message: "enter your operator",
-  choices: ["addition", "substraction", "multiplication", "division"],
+  choices: [
+    "addition",
+    "substraction",
+    "multiplication",
+    "division",
+    "exponent",
+    "reminder",
+  ],
 });
 
 function calculate(
@@ -30,16 +46,25 @@ function calculate(
     console.log(user_input1 * user_input2);
   } else if (user_input3 === "division") {
     console.log(user_input1 / user_input2);
+  } else if (user_input3 === "exponent") {
+    console.log(user_input1 ** user_input2);
+  } else if (user_input3 === "reminder") {
+    console.log(user_input1 % user_input2);
   } else {
     console.log("not a valid operation");
   }
 }
+
 // input no :- 1
-console.log(user_input1.number1);
-// input no :- 2 
-console.log(user_input2.number2);
+console.log("First Number is:", user_input1.number1);
+// input no :- 2
+console.log("Second Number is:", user_input2.number2);
 // your operator.
-console.log(user_input3.operator);
+console.log("The Operator is:", user_input3.operator);
 
 // your answer
-calculate(user_input1.number1, user_input2.number2, user_input3.operator);
+const res = calculate(
+  user_input1.number1,
+  user_input2.number2,
+  user_input3.operator
+);
