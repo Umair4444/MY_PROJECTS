e74fb78b0f752d4ecf7cf84c19bb94fd96fa89f2
Umair4444 <theumairkhan44@gmail.com>
2024-04-12 16:36:26 +0500

    added chalk and loop

--- CLI_CURRENCY_ CONVERTER_00/main.ts
+++ CLI_CURRENCY_ CONVERTER_00/main.ts
@@ -3,6 +3,8 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+let condition = true;
+
 let currency: any = {
   USD: 1, // BASE CURRENCY
   PKR: 278,
@@ -15,56 +17,86 @@ let currency: any = {
   INR: 83.26,
   KWD: 0.31,
 };
+console.log(currency);
 
-let user_input = await inquirer.prompt([
-  {
-    name: "from",
+async function converter() {
+  let option = await inquirer.prompt({
+    name: "user_options",
     type: "list",
-    choices: [
-      "USD",
-      "PKR",
-      "EURO",
-      "GBP",
-      "SAR",
-      "JPY",
-      "AUD",
-      "CNY",
-      "INR",
-      "KWD",
-    ],
-    message: "Please Select the currency you want to convert from: ",
-  },
-  {
-    name: "to",
-    type: "list",
-    choices: [
-      "USD",
-      "PKR",
-      "EURO",
-      "GBP",
-      "SAR",
-      "JPY",
-      "AUD",
-      "CNY",
-      "INR",
-      "KWD",
-    ],
-    message: "Please Select the currency you want to convert to: ",
-  },
-  {
-    name: "amount",
-    type: "number",
-    message: "Please enter the amount you want to convert: ",
-  },
-]);
+    choices: ["Currency Converter", "Forex Exchange Rates", "Exit"],
+    message: "What do you like to do",
+  });
 
-let from_amount = currency[user_input.from];
-let to_amount = currency[user_input.to];
-let amount = user_input.amount;
-let base_amount = amount / from_amount;
-let converted_amount = base_amount * to_amount;
-console.log(
-  chalk.green(
-    `You converted ${amount} ${user_input.from} to ${user_input.to} and now your converted amount is ${converted_amount} ${user_input.to}`
-  )
-);
+  if (option.user_options === "Currency Converter") {
+    let user_input = await inquirer.prompt([
+      {
+        name: "from",
+        type: "list",
+        choices: [
+          "USD",
+          "PKR",
+          "EURO",
+          "GBP",
+          "SAR",
+          "JPY",
+          "AUD",
+          "CNY",
+          "INR",
+          "KWD",
+        ],
+        message: "Please Select the currency you want to convert from: ",
+      },
+      {
+        name: "to",
+        type: "list",
+        choices: [
+          "USD",
+          "PKR",
+          "EURO",
+          "GBP",
+          "SAR",
+          "JPY",
+          "AUD",
+          "CNY",
+          "INR",
+          "KWD",
+        ],
+        message: "Please Select the currency you want to convert to: ",
+      },
+      {
+        name: "amount",
+        type: "number",
+        message: "Please enter the amount you want to convert: ",
+      },
+    ]);
+
+    let from_amount = currency[user_input.from];
+    let to_amount = currency[user_input.to];
+    let amount = user_input.amount;
+    let base_amount = amount / from_amount;
+    let converted_amount = base_amount * to_amount;
+    console.log(
+      chalk.green(
+        `You converted ${amount} ${user_input.from} to ${user_input.to} and now your converted amount is ${converted_amount} ${user_input.to}`
+      )
+    );
+  } else if (option.user_options === "Forex Exchange Rates") {
+    console.log(currency);
+  } else if (option.user_options === "Exit") {
+    console.log(chalk.red("Please come back"));
+    condition = false;
+  }
+}
+
+do {
+  await converter();
+
+  var again = await inquirer.prompt({
+    name: "restart",
+    type: "confirm",
+    message: "If you want to continue press any key and to stop press n or N",
+    default: true,
+  });
+} while (again.restart);
+
+
