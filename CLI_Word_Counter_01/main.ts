174120b07db53cb3a4cde130fc32dae600b9bf65
Umair4444 <theumairkhan44@gmail.com>
2024-04-29 02:20:40 +0500

    CLI word counter program

---
+++ CLI_Word_Counter_01/main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let user_paragraph = await inquirer.prompt({
+  name: "paragraph",
+  type: "input",
+  message: "Write your paraghaph here =>> ",
+});
+
+function word_counter(user_input: any) {
+  //   const inputByUSer = user_input.toString;
+  let word_length = user_input.replace(/\s+/gi, " ").trim();
+  console.log(word_length);
+  // counts words
+  console.log(`Total words are =>  ${word_length.split(" ").length}`);
+
+  // counts characters
+  console.log(
+    `Total characters are => ${word_length.replace(/\s+/gi, "").trim().length}`
+  );
+}
+
+await word_counter(user_paragraph.paragraph);
